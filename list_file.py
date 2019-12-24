1500fbb9d17814f6aa48e8f563b3bbc0d2407bb7
524308450 <524308450@qq.com>
2019-12-24 10:52:48 +0800

    拼接文件路径

--- list_file.py
+++ list_file.py
@@ -7,3 +7,9 @@ root_path = r'/mnt/'
 
 for item in os.walk(root_path):
     print(item)
+
+#拼接文件路径
+for base_path,folder_list,file_list in os.walk(root_path):
+ for file_names in file_list:
+  file_path = os.path.join(base_path,file_names)
+  print(file_path)
