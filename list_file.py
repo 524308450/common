ecba6386d7e2f1cfa3080b9080c2c775d763fcdd
524308450 <524308450@qq.com>
2019-12-24 11:08:55 +0800

    拆分

--- list_file.py
+++ list_file.py
@@ -8,8 +8,11 @@ root_path = r'/mnt/'
 for item in os.walk(root_path):
     print(item)
 
-#拼接文件路径
+#拼接文件路径,python2.7下,不能使用maxspli参数
 for base_path,folder_list,file_list in os.walk(root_path):
  for file_names in file_list:
   file_path = os.path.join(base_path,file_names)
-  print(file_path)
+  file_ext = file_path.rsplit('.',1)
+  print file_path
+  print file_ext
+
