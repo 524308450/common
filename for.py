b1a41621b25d69e294de579f55af41769e72a540
524308450 <524308450@qq.com>
2020-01-03 10:59:48 +0800

    for循环测试下的continue解释

---
+++ for.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+###continue 语句跳出本次循环，而break跳出整个循环###
+###continue 语句用来告诉Python跳过当前循环的剩余语句，然后继续进行下一轮循环###
+
+for i in 'Python':
+ if i == 'h':
+  continue
+ print '当前字母 :', i
+
+
+var = 10
+while var > 0:
+ var = var -1
+ if var == 5:
+  continue
+ print '当前变量值 :', var
+
+print "Good Bye!"
