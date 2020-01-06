cee9b04e52d385a5f73863bd2df6ca6635973662
524308450 <524308450@qq.com>
2020-01-06 12:52:06 +0800

    for循环语句not的用法

--- cnt.py
+++ cnt.py
@@ -37,3 +37,43 @@ print "Good Bye!"
 当前变量值 : 0
 Good Bye!
 """
+
+n = 0
+while n < 10:
+    n = n + 1
+#    if n % 2 == 0:      # 如果n是偶数，执行continue语句
+    if  n%2:      # 如果n是奇数，执行continue语句
+        continue        # continue语句会直接继续下一轮循环，后续的print()语句不会执行
+    print(n)
+
+"""
+2
+4
+6
+8
+10
+"""
+
+
+
+var = 10
+
+while var > 0:
+    var = var -1
+    if var == 5 or var == 8:
+        continue
+    print '当前值 :', var
+print "Good bye!"
+
+"""
+当前值 : 9
+当前值 : 7
+当前值 : 6
+当前值 : 4
+当前值 : 3
+当前值 : 2
+当前值 : 1
+当前值 : 0
+Good bye!
+"""
+
